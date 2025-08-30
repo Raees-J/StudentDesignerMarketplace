0f1dc5b011b9ee7efb28633992b565fa67b8dfb9
Aristide Izabkora <220345821@mycput.ac.za>
2025-08-30 20:05:52 +0200

    Added Testing to review

--- src/main/java/za/ac/cput/repository/ReviewRepository.java
+++ src/main/java/za/ac/cput/repository/ReviewRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Review;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, String> {
 
-
+    List<Review> findByCustomerID(String customerID);
+    List<Review> findByProductID(String productID);
+    List<Review> findByRating(int rating);
+    List<Review> findByCommentContainingIgnoreCase(String keyword);
 }
 
